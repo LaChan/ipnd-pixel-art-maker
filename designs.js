b1a69a9d5ba958f0830a3eceb3347b53310a673d
LaChan <lachlan.hope@gmail.com>
2018-12-13 17:39:22 +1100

    3rd commit

--- designs.js
+++ designs.js
@@ -12,11 +12,11 @@ const colorInput = document.querySelector('#colorPicker');
 
 const sizeInput = document.querySelector('#sizePicker');
 
-var color = "#ffffff"
+let color = "#ffffff";
 
-let inputHeight = 0
+let inputHeight = 0;
 
-let inputWidth = 0
+let inputWidth = 0;
 
 //Setup Event Listeners
 sizeInput.addEventListener('submit', function(evt){
@@ -33,32 +33,31 @@ colorInput.addEventListener('input',  function(evt){
 //Function to set color of an individual cell
 function setCellColor(evt){
   evt.target.style.backgroundColor = color;
-}
+};
 
 //Function to create the drawing grid
 function makeGrid(ih, iw) {
 
-    //setup table element for grid, with positioning and styling
-    let grid = document.createElement('table');
-    grid.setAttribute("id", "drawing_grid")
-    grid.addEventListener('click', setCellColor);
+  //setup table element for grid, with positioning and styling
+  let grid = document.createElement('table');
+  grid.setAttribute("id", "drawing_grid");
+  grid.addEventListener('click', setCellColor);
 
-    //actually draw the cells
-    for(var i = 0; i < ih; i++) {
-      let tr = grid.insertRow();
-      for(var j = 0; j < iw; j++) {
-        let td = tr.insertCell(j);
-        td.style.backgroundColor = color;
+  //actually draw the cells
+  for(var i = 0; i < ih; i++) {
+    let tr = grid.insertRow();
+    for(var j = 0; j < iw; j++) {
+      let td = tr.insertCell(j);
+      td.style.backgroundColor = color;
       }
-    }
-
-    //check for an existing table with drawing_grid id, and remove if present
-    let tbl = document.querySelector('#drawing_grid');
-    if (document.contains(tbl)){
-      bdy.removeChild(tbl)
-      bdy.appendChild(grid);
-    } else {
-      bdy.appendChild(grid);
-    } 
   }
-  
+
+  //check for an existing table with drawing_grid id, and remove if present
+  let tbl = document.querySelector('#drawing_grid');
+  if (document.contains(tbl)){
+    bdy.removeChild(tbl);
+    bdy.appendChild(grid);
+  } else {
+    bdy.appendChild(grid);
+  }
+}
